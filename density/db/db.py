12a2392a94091ff758191bd324dec34560d93fc6
swbae31@gmail.com <swbae31@gmail.com>
2014-11-14 15:04:28 -0500

    Defined more db functions and implemented db.py to the routes.

--- density/db/db.py
+++ density/db/db.py
@@ -13,9 +13,58 @@ def get_latest_data(cursor):
     :return: list of dicts representing each row from the db
     :rtype: list of dict
     """
-    query = """SELECT * FROM {table_name} WHERE dump_time=(SELECT MAX("
-             "dump_time) FROM {table_name});""".format(table_name=TABLE_NAME)
+    query = """SELECT *
+               FROM {table_name}
+               WHERE dump_time=(
+                   SELECT MAX(dump_time)
+                   FROM {table_name}
+               )
+               ;""".format(table_name=TABLE_NAME)
     cursor.execute(query)
+    return cursor.fetchall()
+
+
+def get_latest_group_data(cursor, group_id):
+    """
+    Gets latest dump of data for the specified group.
+
+    :param cursor: cursor for our DB
+    :param int group_id: id of the requested group
+    :return:  list of dictionaries representing the rows corresponding to the
+    query
+    :rtype: list of dict
+    """
+
+    query = """SELECT *
+               FROM {table_name}
+               WHERE dump_time=(
+                   SELECT MAX(dump_time)
+                   FROM {table_name}
+               ) AND group_id=%s
+               ;""".format(table_name=TABLE_NAME)
+    cursor.execute(query, [group_id])
+    return cursor.fetchall()
+
+
+def get_latest_building_data(cursor, parent_id):
+    """
+    Gets latest dump of data for the specified building.
+
+    :param cursor: cursor for our DB
+    :param int parent_id: id of the requested building
+    :return:  list of dictionaries representing the rows corresponding to the
+    query
+    :rtype: list of dict
+    """
+
+    query = """SELECT *
+               FROM {table_name}
+               WHERE dump_time=(
+                   SELECT MAX(dump_time)
+                   FROM {table_name}
+               ) AND parent_id=%s
+               ;""".format(table_name=TABLE_NAME)
+    cursor.execute(query, [parent_id])
     return cursor.fetchall()
 
 
@@ -24,7 +73,7 @@ def get_window_based_on_group(cursor, group_id, start_time, end_time):
     Gets all data for a group within a window. It will return the latest 100
     rows starting with the most recent ones.
 
-    :param cursor:
+    :param cursor: cursor for our DB
     :param int group_id: id of the group requested
     :param str start_time: start time of window
     :param str end_time: end time of the window
@@ -32,14 +81,16 @@ def get_window_based_on_group(cursor, group_id, start_time, end_time):
     query
     :rtype: list of dict
     """
-    query = """SELECT * FROM {}
-               WHERE (dump_time >= %s
-                      AND dump_time <= %s)
-                      AND group_id=%s
+    query = """SELECT *
+               FROM {table_name}
+               WHERE (
+                    dump_time >= %s
+                    AND dump_time <= %s
+               ) AND group_id=%s
+               ORDER BY dump_time DESC
                LIMIT %s
-               ORDER BY dump_time DESC;""".format(TABLE_NAME)
-    cursor.execute(query, [start_time, end_time, group_id,
-                   QUERY_LIMIT])
+               ;""".format(table_name=TABLE_NAME)
+    cursor.execute(query, [start_time, end_time, group_id, QUERY_LIMIT])
     return cursor.fetchall()
 
 
@@ -56,12 +107,16 @@ def get_window_based_on_parent(cursor, parent_id, start_time, end_time):
     query
     :rtype: list of dict
     """
-    query = """SELECT * FROM {}
-               WHERE (dump_time >= %s
-                      AND dump_time <= %s)
-                      AND parent_id=%s
+    query = """SELECT *
+               FROM {table_name}
+               WHERE (
+                    dump_time >= %s
+                    AND dump_time <= %s
+               ) AND parent_id=%s
+               ORDER BY dump_time DESC
                LIMIT %s
-               ORDER BY dump_time DESC;""".format(TABLE_NAME)
-    cursor.execute(query, [start_time, end_time, parent_id,
-                   QUERY_LIMIT])
+               ;""".format(table_name=TABLE_NAME)
+    cursor.execute(query, [start_time, end_time, parent_id, QUERY_LIMIT])
     return cursor.fetchall()
+
+
