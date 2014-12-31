762a864062974084cfa5550804df5b4d1ae77fde
Nate Brennand <natebrennand@gmail.com>
2014-12-31 12:50:30 -0500

    fix failing tests

--- density/tests/test_latest.py
+++ density/tests/test_latest.py
@@ -11,11 +11,10 @@ class TestLatestRoute(template.TestingTemplate):
         json_resp = json.loads(resp.data)
         self.assertEqual(22, len(json_resp['data']))
 
-
     def test_number_empty_rooms(self):
         resp = self.app.get('/latest')
         self.assertEqual(200, resp.status_code)
 
         json_resp = json.loads(resp.data)
-        self.assertEqual(2, len([r for r in json_resp['data'] if not r['client_count']]))
-
+        self.assertEqual(2, len([r for r in json_resp['data']
+                                 if not r['client_count']]))
