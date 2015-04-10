3283e0a0173c6e2067a58aa8ae5bba549a49d20e
evantarrh <evantarrh@gmail.com>
2015-04-10 14:16:26 -0400

    more cleanup, adding style guide to readme

--- density/static/map.js
+++ density/static/map.js
@@ -13,90 +13,87 @@ document.addEventListener('DOMContentLoaded', function() {
     var initialOffsetLeft = map.getBoundingClientRect().left + window.scrollX;
     
     //Create a mapping from parent_id --> text representation
-    var parentToText = {};
+    var buildingStrings = {};
     
     //Create a mapping from parent_id --> list full %s for elements inside (e.g. floors of a building)
-    var parentToList = {};
+    var buildingFloors = {};
 
     //Create a mapping from parent_id --> query (http://....hours)
-    var parentToQuery = {};
+    var buildingHours = {};
     
-    for (var i = 0; i < locations.length; i++) { 
-      if (locations[i].name != 'Butler Library stk') { //We're skipping this out of non-use]
-        if (parentToList[locations[i].parentId] == null) {
-          parentToList[locations[i].parentId] = {};
+    locations.forEach(function(location) {
+      if (location.name != 'Butler Library stk') { //We're skipping this out of non-use]
+        if (buildingFloors[location.parentId] == null) {
+          buildingFloors[location.parentId] = {};
         }
-        if (parentToText[locations[i].parentId] == null) {
-          parentToText[locations[i].parentId] = '<b>' + locations[i].parentName + '</b><br />' +
+        if (buildingStrings[location.parentId] == null) {
+          buildingStrings[location.parentId] = '<b>' + location.parentName + '</b><br />' +
             '<em>Click to view hours</em><br /><br />';
         }
-        if (parentToQuery[locations[i].parentId] == null) {
-          parentToQuery[locations[i].parentId] = 'https://www.google.com/#q=' + escape('Columbia University ' + locations[i].parentName + ' hours');
+        if (buildingHours[location.parentId] == null) {
+          buildingHours[location.parentId] = 'https://www.google.com/#q=' + escape('Columbia University ' + location.parentName + ' hours');
         }
         
-        parentToList[locations[i].parentId][locations[i].name] = locations[i].fullness;
-        parentToText[locations[i].parentId] += locations[i].name + ': ' + locations[i].fullness + '%' + '<br />';
+        buildingFloors[location.parentId][location.name] = location.fullness;
+        buildingStrings[location.parentId] += location.name + ': ' + location.fullness + '%' + '<br />';
       }
-    }
+    });
 
     //Iterate through mappings keys to set opacity for the element by fullness and better data on mouseover
-    var keySet = Object.keys(parentToList);
+    var buildings = Object.keys(buildingFloors);
 
-    for (var key in keySet) {
-      var keyValue = keySet[key];
-      var list = parentToList[keyValue];
+    for (var i in buildings) {
+      var building = buildings[i];
+      var list = buildingFloors[building];
       
       //find average fullness for location (this is for locations w/ multiple floors)
-      var count = 0;
       var totalFullness = 0;
       for (var entry in list){
-        totalFullness += parentToList[keyValue][entry];
-        count++;
+        totalFullness += buildingFloors[building][entry];
       }
 
       //number of pixels offset to separate popup from mouse
       var mouseOffset = 4;
+      var numFloorsInBuilding = Object.keys(list).length;
+
+      var percent = (totalFullness / numFloorsInBuilding); //Should be from [0, 100]
+      var buildingElement = innerSvg.getElementById('parent_' + building); //e.g. parent_43
+      if(buildingElement != null) {
+        buildingElement.style.opacity = percent / 100;
+      }
       
-      if (count != 0) {
-        var percent = (totalFullness / count); //Should be from [0, 100]
-        var buildingElement = innerSvg.getElementById('parent_' + keyValue); //e.g. parent_43
-        if(buildingElement != null) {
-          buildingElement.style.opacity = percent / 100;
-        }
-        
-        //opens new window with google search for hours
-        buildingElement.onmousedown = function(event) {
-          var location = this.id.replace('parent_', '');
-          var query = parentToQuery[location];
-          window.open(query);
-        }
+      //opens new window with google search for hours
+      buildingElement.onmousedown = function(event) {
+        var location = this.id.replace('parent_', '');
+        var query = buildingHours[location];
+        window.open(query);
+      }
 
-        //displays popup at location of mouse
-        buildingElement.onmouseenter = function(event) {
-          popup.style.visibility = 'visible';
-          var leftPosition = (initialOffsetLeft + event.clientX - 
-            (popup.offsetWidth/2)).toString() + 'px'; // divide by 2 centers it (half of width)
-          var topPosition = (initialOffsetTop - mouseOffset + event.clientY -
-            (popup.offsetHeight)).toString() + 'px';
-          popup.style.top = topPosition;
-          popup.style.left = leftPosition;
-          var location = this.id.replace('parent_', '');
-          popup.innerHTML = parentToText[location];
-        }
+      //displays popup at location of mouse
+      buildingElement.onmouseenter = function(event) {
+        popup.style.visibility = 'visible';
+        var leftPosition = (initialOffsetLeft + event.clientX - 
+          (popup.offsetWidth/2)).toString() + 'px'; // divide by 2 centers it (half of width)
+        var topPosition = (initialOffsetTop - mouseOffset + event.clientY -
+          (popup.offsetHeight)).toString() + 'px';
+        popup.style.top = topPosition;
+        popup.style.left = leftPosition;
+        var location = this.id.replace('parent_', '');
+        popup.innerHTML = buildingStrings[location];
+      }
 
-        buildingElement.onmouseout = function() {
-          popup.style.visibility = 'hidden';
-        }
+      buildingElement.onmouseout = function() {
+        popup.style.visibility = 'hidden';
+      }
 
-        //moves popup with mouse
-        buildingElement.onmousemove = function(event) {
-          var leftPosition = (initialOffsetLeft + event.clientX - 
-            (popup.offsetWidth/2)).toString() + 'px'; // divide by 2 centers it (half of width)
-          var topPosition = (initialOffsetTop - mouseOffset + event.clientY -
-            (popup.offsetHeight)).toString() + 'px';
-          popup.style.top = topPosition;
-          popup.style.left = leftPosition;
-        }
+      //moves popup with mouse
+      buildingElement.onmousemove = function(event) {
+        var leftPosition = (initialOffsetLeft + event.clientX - 
+          (popup.offsetWidth/2)).toString() + 'px'; // divide by 2 centers it (half of width)
+        var topPosition = (initialOffsetTop - mouseOffset + event.clientY -
+          (popup.offsetHeight)).toString() + 'px';
+        popup.style.top = topPosition;
+        popup.style.left = leftPosition;
       }
     }
   });
