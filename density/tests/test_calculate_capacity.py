04ed498625eb2cde26854ffd8f6bc7ea2d5ba675
blvns <tlb2145@columbia.edu>
2015-05-01 15:02:34 -0400

    Encapsulating the logic for map() in calculate_capacity()

--- density/tests/test_calculate_capacity.py
+++ density/tests/test_calculate_capacity.py
@@ -2,19 +2,22 @@ import unittest
 
 from density import calculate_capacity
 
-
-class TestCalculateCapacity(unittest.TestCase):
+Class TestCalculateCapacity(unittest.TestCase):
 
     # testing logic in calulate_capacity()
     def test_calculate_capacity(self):
 
         # test version of cap_data
         cap_test = [
-            {'capacity': 100, 'group_name': 'Butler Library 6'},
+            {'capacity': 100, 'group_name': 'Butler Library 6',
+             'parent_id': 103, 'parent_name': 'Butler'},
             # test stk ->stacks
-            {'capacity': 255, 'group_name': 'Butler Library stk'},
-            {'capacity': 75, 'group_name': "JJ's Place"},
-            {'capacity': 150, 'group_name': 'Starr East Asian Library'}
+            {'capacity': 255, 'group_name': 'Butler Library stk',
+             'parent_id': 103, 'parent_name': 'Butler'},
+            {'capacity': 75, 'group_name': "JJ's Place",
+             'parent_id': 75, 'parent_name': 'John Jay'},
+            {'capacity': 150, 'group_name': 'Starr East Asian Library',
+             'parent_id': 62, 'parent_name': 'East Asian Library'}
         ]
 
         # test version of cur_data
@@ -27,14 +30,19 @@ class TestCalculateCapacity(unittest.TestCase):
 
         # test (expected) version of locations
         loc_accurate = [
-            {'name': 'Butler Library 6', 'fullness': 78},
-            {'name': 'Butler Library Stacks', 'fullness': 39},
-            {'name': "JJ's Place", 'fullness': 100},
-            {'name': 'Starr East Asian Library', 'fullness': 10}
+            {'name': 'Butler Library 6', 'fullness': 78,
+             'parent_id': 103, 'parent_name': 'Butler'},
+            {'name': 'Butler Library Stacks', 'fullness': 39,
+             'parent_id': 103, 'parent_name': 'Butler'},
+            {'name': "JJ's Place", 'fullness': 100,
+             'parent_id': 75, 'parent_name': 'John Jay'},
+            {'name': 'Starr East Asian Library', 'fullness': 10,
+             'parent_id': 62, 'parent_name': 'East Asian Library'}
         ]
 
         loc_test = calculate_capacity(cap_test, cur_test)
 
+        # need to add test for parent_id and parent_name
         for i in range(len(loc_accurate)):
             self.assertEqual(loc_accurate[i]['name'], loc_test[i]['name'])
             self.assertEqual(loc_accurate[i]['fullness'],
