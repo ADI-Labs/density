a4e87484474ad604cd4df57fd23e978c0a8ff6d3
Alan Du <alanhdu@gmail.com>
2016-02-24 22:04:18 -0500

    Remove unittest boilerplate
    Thanks pytest!

--- density/tests/test_auth.py
+++ density/tests/test_auth.py
@@ -1,22 +1,16 @@
-import density
-import unittest
 import re
+import density
 
+def test_email_regex():
+    """ Tests the regex used to filter Oauth applicants """
+    cases = [
+        ('nsb2142@columbia.edu', True),
+        ('nsb2142@columbia.eduABC', False),
+        ('@@nsb2142@columbia.edu', False),
+        ('jae@cs.columbia.edu', True),
+        ('nsb2142@barnard.edu', True),
+        ('nsb2142@yale.edu', False),
+    ]
 
-class TestAuth(unittest.TestCase):
-
-    def test_email_regex(self):
-        """ Tests the regex used to filter Oauth applicants """
-        cases = [
-            ('nsb2142@columbia.edu', True),
-            ('nsb2142@columbia.eduABC', False),
-            ('@@nsb2142@columbia.edu', False),
-            ('jae@cs.columbia.edu', True),
-            ('nsb2142@barnard.edu', True),
-            ('nsb2142@yale.edu', False),
-        ]
-
-        for email, res in cases:
-            self.assertEqual(
-                bool(re.match(density.CU_EMAIL_REGEX, email)),
-                res)
+    for email, match in cases:
+        assert bool(re.match(density.CU_EMAIL_REGEX, email)) == match
