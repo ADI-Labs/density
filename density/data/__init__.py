3a246578ee0fb56b3de5bb50774d5965718fd400
Sam Cohen <samsam489@gmail.com>
2016-04-01 14:54:43 -0400

    Add doctring to db_to_pandas functions

--- density/data/__init__.py
+++ density/data/__init__.py
@@ -3,6 +3,27 @@ import pandas as pd
 
 
 def db_to_pandas(conn):
+	""" Return occupancy data as pandas dataframe
+
+	column dtypes:
+		group_id: int64
+		group_name: category
+		parent_id: int64
+		parent_name: category
+		client_count: int64
+	index: DateTimeIndex -- dump_time
+
+	Parameters
+	----------
+	conn: psycopg2.extensions.connection
+
+
+	Returns
+	_______
+
+	pandas.DataFrame
+
+	"""
 
 	df = pd.read_sql('SELECT * FROM density_data', conn) \
 		   .set_index("dump_time") \
@@ -10,3 +31,17 @@ def db_to_pandas(conn):
 				   parent_name=lambda df: df["parent_name"].astype('category'))
 
 	return df
+
+
+
+
+
+
+
+
+
+
+
+
+
+
