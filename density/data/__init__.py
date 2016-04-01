f377564e10876f76b867f8e5c03bdfa745a16458
Jin Peng <jin.peng@columbia.edu>
2016-04-01 17:40:32 -0400

    basic visualization with bokeh

--- density/data/__init__.py
+++ density/data/__init__.py
@@ -1,4 +1,10 @@
+from bokeh.plotting import figure
+from pandas import PeriodIndex
+
 import pandas as pd
+
+
+PANTONE_292 = (105, 179, 231)
 
 def db_to_pandas(conn):
     """ Return occupancy data as pandas dataframe
@@ -28,3 +34,30 @@ def db_to_pandas(conn):
                    parent_name=lambda df: df["parent_name"].astype('category'))
 
     return df
+
+
+def plot_prediction_point_estimate(series, predictor):
+
+    future_dts = PeriodIndex(start=series.index[-1], freq='15T',
+                             periods=24 * 4)
+    predictions = pd.Series(predictor(future_dts),
+                            index=future_dts.to_datetime())
+
+    p = figure(x_axis_type="datetime")
+    p.line(series.index, series, color="dodgerblue", line_width=3,
+           line_cap="round")
+    p.line(predictions.index, predictions, color="crimson", line_width=3,
+           line_dash="dashed", line_cap="round")
+
+    p.xaxis.axis_label = "Time of Day"
+    p.xaxis.axis_line_width = 3
+    p.xaxis.axis_line_color = PANTONE_292
+    p.xaxis.major_label_text_color = PANTONE_292
+
+    p.yaxis.axis_label = "Capacity"
+    p.yaxis.axis_line_color = PANTONE_292
+    p.yaxis.major_label_text_color = PANTONE_292
+    p.yaxis.major_label_orientation = "vertical"
+    p.yaxis.axis_line_width = 3
+
+    return p
