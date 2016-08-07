56afe7a170ca4673acb7caa2b7731881c374d8ce
Alex Yu <afy2103@columbia.edu>
2016-08-06 17:17:17 -0700

    Removed extra test in whitelist 	modified:   whitelist.py

--- whitelist.py
+++ whitelist.py
@@ -23,31 +23,6 @@ figure.yaxis.axis_line_width
 app.json_encoder
 
 # Ignore all Flask routes
-# Because vulture only performs static analysis, this file is used
-# by vulture as a whitelist module and passed as a parameter when
-# running vulture so that it'll know that certain methods are being
-# used.
-
-import density
-from bokeh import figure
-
-app = density.app
-
-# Ignore bokeh figure attribute assignment
-figure.xaxis.axis_label
-figure.xaxis.axis_line_width
-figure.xaxis.axis_line_color
-figure.xaxis.major_label_text_color
-figure.yaxis.axis_label
-figure.yaxis.axis_line_color
-figure.yaxis.major_label_text_color
-figure.yaxis.major_label_orientation
-figure.yaxis.axis_line_width
-
-# Ignore Flask app attribute assignment
-app.json_encoder
-
-# Ignore all Flask routes
 density.get_connections
 density.log_outcome
 density.page_not_found
