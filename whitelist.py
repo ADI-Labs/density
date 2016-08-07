09447ba19192d05da38831708f0e23c245db43d3
Alex Yu <afy2103@columbia.edu>
2016-08-06 17:06:29 -0700

    Reverting whitelist change 	modified:   whitelist.py

--- whitelist.py
+++ whitelist.py
@@ -23,5 +23,44 @@ figure.yaxis.axis_line_width
 app.json_encoder
 
 # Ignore all Flask routes
-routes = [app.view_functions[rule.endpoint] for rule in
-          app.url_map.iter_rules()]
+# Because vulture only performs static analysis, this file is used
+# by vulture as a whitelist module and passed as a parameter when
+# running vulture so that it'll know that certain methods are being
+# used.
+
+import density
+from bokeh import figure
+
+app = density.app
+
+# Ignore bokeh figure attribute assignment
+figure.xaxis.axis_label
+figure.xaxis.axis_line_width
+figure.xaxis.axis_line_color
+figure.xaxis.major_label_text_color
+figure.yaxis.axis_label
+figure.yaxis.axis_line_color
+figure.yaxis.major_label_text_color
+figure.yaxis.major_label_orientation
+figure.yaxis.axis_line_width
+
+# Ignore Flask app attribute assignment
+app.json_encoder
+
+# Ignore all Flask routes
+density.get_connections
+density.log_outcome
+density.page_not_found
+density.internal_error
+density.home
+density.about
+density.predict
+density.docs
+density.building_info
+density.auth
+density.redirect_uri
+density.get_day_group_data
+density.get_day_building_data
+density.get_window_group_data
+density.get_window_building_data
+density.map
