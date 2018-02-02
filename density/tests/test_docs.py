da3e29a79bb182e8b58dded630de2cd70595363c
Ignacio Ramirez <ignacioramirez@dyn-160-39-170-245.dyn.columbia.edu>
2018-02-02 16:44:39 -0500

    Tests /docs

--- density/tests/test_docs.py
+++ density/tests/test_docs.py
@@ -1,4 +1,10 @@
 import copy
 import json
 
-import desnity
+import density
+
+
+def test_docs(app, auth_header):
+    resp = app.get("/docs", headers=auth_header)
+
+    assert resp.status_code == 200
