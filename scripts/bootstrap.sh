0646bec231ec853dacb52df2a4aff4cefae9543d
Bill Chen <billchen99@gmail.com>
2018-02-02 15:31:02 -0500

    replaced with cynthia's bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-sudo -u postgres createuser --superuser adi
-sudo -u postgres createdb -E UTF8 -l en_US.UTF8 -T template0 -O adi density
-sudo -u postgres psql -d density <<EOL
+createuser --superuser adi
+createdb -E UTF8 -T template0 -O adi density
+psql -d density <<EOL
 ALTER ROLE adi WITH PASSWORD 'password';
 EOL
-sudo -u postgres psql density < scripts/dump.sql
+psql density < scripts/dump.sql
 
 echo
 echo
