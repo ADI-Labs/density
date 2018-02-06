2f75694cb66a760411ee5defb6cdaf1054a9e33f
cynthiamao98 <32347565+cynthiamao98@users.noreply.github.com>
2018-02-06 17:24:00 -0500

    Update test_latest.py
    fix test_latest_group_data

--- density/tests/test_latest.py
+++ density/tests/test_latest.py
@@ -16,15 +16,16 @@ def test_latest_data(app, auth_header):
                               'percent_full', 'parent_id'}
         
 
-def test_good_latest_group_data(app, auth_header):
+def test_latest_group_data(app, auth_header):
     resp = app.get("/latest/group/148", headers=auth_header)
     body = json.loads(resp.data.decode())
 
     assert resp.status_code == 200
-    for row in body["data"]:
-        assert row.keys() == {'client_count', 'building_name', 'client_count',
-                              'dump_time', 'group_name', 'group_id',
-                              'percent_full', 'parent_id'}
+    assert len(body["data"]) == 1
+    assert body["data"][0]["building_name"] == "Avery"
+    assert body["data"][0]["group_name"] == "Architectural and Fine Arts Library 2"
+    assert body["data"][0]["parent_id"] == 146
+    assert body["data"][0]["group_id"] == 148
         
 
 def test_annotate_fullness_percentage():
