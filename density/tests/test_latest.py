9b403f997e388f019278f8d68c7d03a757a83948
cynthiamao98 <32347565+cynthiamao98@users.noreply.github.com>
2018-02-06 22:21:47 -0500

    Add test_latest_building_data

--- density/tests/test_latest.py
+++ density/tests/test_latest.py
@@ -27,6 +27,19 @@ def test_latest_group_data(app, auth_header):
         "Architectural and Fine Arts Library 2")
     assert body["data"][0]["parent_id"] == 146
     assert body["data"][0]["group_id"] == 148
+    
+    
+def test_latest_building_data(app, auth_header):
+    resp = app.get("/latest/building/103", headers=auth_header)
+    body = json.loads(resp.data.decode())
+
+    assert resp.status_code == 200
+    assert len(body["data"]) == 6
+    for row in body["data"]:
+        assert row.keys() == {'client_count', 'building_name', 'client_count',
+                              'dump_time', 'group_name', 'group_id',
+                              'percent_full', 'parent_id'}
+        assert row["building_name"] == "Butler"
         
 
 def test_annotate_fullness_percentage():
