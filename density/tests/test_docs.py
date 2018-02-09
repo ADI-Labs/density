5d85d79e25cb7d781090b4d5abdd5f9e7fceea66
cynthiamao98 <32347565+cynthiamao98@users.noreply.github.com>
2018-02-09 13:14:16 -0500

    fix format

--- density/tests/test_docs.py
+++ density/tests/test_docs.py
@@ -1,8 +1,4 @@
-import copy
 import json
-
-import density
-
 
 def test_docs(app, auth_header):
     resp = app.get("/docs", headers=auth_header)
@@ -14,7 +10,7 @@ def test_building_info(app, auth_header):
 	body = json.loads(resp.data.decode())
 
 	assert resp.status_code == 200
-	assert len(body['data']) == 23
+        assert len(body['data']) == 23
 
 	for row in body["data"]:
-		assert row.keys() == {'group_id', 'group_name', 'parent_id', 'parent_name'}
+        assert row.keys() == {'group_id', 'group_name', 'parent_id', 'parent_name'}
