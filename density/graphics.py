911a3b1ee9ecfb489624148e972a28d56a69b675
cynthiamao98 <32347565+cynthiamao98@users.noreply.github.com>
2018-02-16 14:54:12 -0500

    create graphic.py
    raw version

---
+++ density/graphics.py
@@ -0,0 +1,9 @@
+from bokeh.plotting import figure
+import pandas as pd
+from pandas import PeriodIndex, DataFrame, Series
+
+def create_figure(building, series):
+    return
+
+def create_all_buildings(df):
+    generator = df.iterrows()
