fa97669bc4e8ccc4ec095a5ba5ee8dc41635b219
Shazmin Mahmud <sm4423@columbia.edu>
2018-02-16 15:45:17 -0500

    Somewhat able to get row from DataFrame

--- density/graphics.py
+++ density/graphics.py
@@ -4,21 +4,27 @@ from pandas import PeriodIndex, DataFrame, Series
 
 def create_figure(building, predictions):
 
-    for time, prediction in predictions.iteritems():
-    	print(time, ": ", predictions)
+	for time, prediction in predictions.iteritems():
+		print("building:", building, "time:", time, "prediction:", prediction)
+
 
 def create_all_buildings(df):
     
-    #  building = index of row
-    #  predictions = Series of columns with predictions
+    #  building = index of row (String)
+    #  predictions = Series of columns with predictions (double va)
+
+    index = 0
     for building, predictions in df.iterrows():
     	create_figure(building, predictions)
+    	index = index + 1
 
 def phony_data():
-	d = {'one' : pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
-		'two' : pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])}
+	d = {'12am' : pd.Series([1, 2, 3, 4, 5, 6], index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
+		'1am' : pd.Series([2, 4, 3, 4, 5, 6], index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
+		'2am' : pd.Series([3, 6, 3, 4, 5, 6], index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
+		'3am' : pd.Series([4, 8, 3, 4, 5, 6], index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']), }
 
 	return pd.DataFrame(d)
 
-#  create_all_buildings(phony_data())
+create_all_buildings(phony_data())
 print(phony_data())
