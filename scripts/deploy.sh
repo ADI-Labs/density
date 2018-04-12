0f1434c629e42c7894ee96b3d1b78d30ea1fb5de
Alan Du <alanhdu@gmail.com>
2018-04-12 00:06:50 -0400

    Add deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+ssh travis@adicu.com <<'ENDSSH'
+echo   "=================================="
+echo   "Deployment Starting"
+echo   "----------------------------------"
+echo   "Progress:"
+printf "Deploying density.adicu.com          "
+
+cd /srv/density/www
+git fetch && git reset --hard origin/deploy
+
+docker build -t density .
+docker kill density
+docker rm density
+docker run \
+    --net=host \
+    --restart=always \
+    -v /srv/density/www/../logs:/opt/logs \
+    -d --name="density" density
+
+printf "Done\n"
+exit
+ENDSSH
