95f6c7822ee3e7a41c5ff3af2f962471ca12ad1d
Shazmin Mahmud <sm4423@columbia.edu>
2018-04-13 14:20:32 -0400

    test_graphics formatting

--- density/tests/test_graphics.py
+++ density/tests/test_graphics.py
@@ -1,33 +1,38 @@
 import pandas as pd
-import pytest
-
 from pandas import DataFrame, PeriodIndex, Series
+
+import pytest
 
 from density import graphics
 
 def test_create_prediction_plot():
-    time = []
+    times = []
+    time_index = []
 
     for x in range(0, 24):
-        time.append(x * 10)
+        times.append(x * 10)
 
     for x in range(0, 24):
         time_index.append(str(x))
 
-    seriesTime = pd.Series(time, index=time_index)
+    seriesTime = pd.Series(times, index=time_index)
 
     time, predictions = (pd.PeriodIndex(time_index), seriesTime)
 
     graphics.create_prediction_plot(time, predictions)
 
 def test_create_all_buildings():
-    d = {'1': pd.Series([1, 2, 3, 4, 5, 1], index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
-        '2': pd.Series([2, 4, 3, 4, 5, 2], index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
-        '3': pd.Series([3, 6, 3, 4, 5, 3], index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
-        '4': pd.Series([4, 8, 3, 4, 5, 4], index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']), }
+    d = {'1': pd.Series([1, 2, 3, 4, 5, 1],
+        index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
+        '2': pd.Series([2, 4, 3, 4, 5, 2],
+        index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
+        '3': pd.Series([3, 6, 3, 4, 5, 3],
+        index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']),
+        '4': pd.Series([4, 8, 3, 4, 5, 4],
+        index=['lerner 1', 'lerner 2', 'lerner 3', 'butler 1', 'butler 2', 'butler 3']) }
 
     all_buildings = pd.DataFrame(d)
 
     script, divs = graphics.create_all_buldings(all_buildings)
 
-    assert len(divs) == 6 #  makes all div files
+    assert len(divs) == 6  # makes all div files
