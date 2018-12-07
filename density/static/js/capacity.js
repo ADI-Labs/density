1047f4c983ff1215ee3cd88f0089bb2623a70e38
Kenneth Yuan <kenny.k.yuan@gmail.com>
2018-12-07 13:44:25 -0500

    Added UI for feedback.

--- density/static/js/capacity.js
+++ density/static/js/capacity.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
     $('#alert-close').click(function() {
         $('.alert-container').css({maxHeight: 0, opacity: 0});
     });
+    function closeFeedback() {
+    	$('#feedback').hide();
+    }
+    function openFeedback(name, percent) {
+    	$('#feedback #building').html(name);
+    	$('#feedback #percentage').html(percent);
+    	$('#feedback').show();
+    }
+    function sendFeedback() {
+    	// Do something
+    }
 });
